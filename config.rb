43e5f0edb613331386d67c5c304646121b6117aa
Robbie Clutton <robert.clutton@gmail.com>
2013-07-23 18:03:18 -0400

    enable livereload

--- config.rb
+++ config.rb
@@ -38,7 +38,7 @@ require "susy"
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
